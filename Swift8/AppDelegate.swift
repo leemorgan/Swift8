dab6a2ce7c18dd73798cc7159d67ceba94cbd5f8
Lee Morgan <leemorgan@me.com>
2015-02-24 09:09:30 -0500

    Initial Commit

---
+++ Swift8/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Swift8
+//
+//  Created by Lee Morgan on 2/24/15.
+//  Copyright (c) 2015 Lee Morgan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
